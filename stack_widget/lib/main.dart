19f3c16c3ebee9c0623f148d8d870d3237585048
ankitsingh0606 <ankitsingh10026@gmail.com>
2023-01-25 13:02:24 +0530

    learn Stack Widget

---
+++ stack_widget/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home:MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Hello"),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.blueGrey,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.green,
+          )
+        ],
+      )
+    );
+  }
+}
