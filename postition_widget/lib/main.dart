ff4c1dd71448582e4577466738aeedc2950b8873
ankitsingh0606 <ankitsingh10026@gmail.com>
2023-01-28 12:05:51 +0530

    learn setstate to set the variable value

--- postition_widget/lib/main.dart
+++ postition_widget/lib/main.dart
@@ -31,24 +31,104 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomeState extends State<MyHomePage>{
   var count =0;
+  var no1Controller = TextEditingController();
+  var no2Controller = TextEditingController();
+  var result ="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Stateful Widget")
+        title: Text("Basic Calculation")
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Count : $count', style: TextStyle(fontSize: 34),),
-            ElevatedButton(onPressed: (){
-              setState(() {
-                count++;
-                print(count);
-              });
-            }, child: Text("Clicked me"))
-          ],
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text('Count : $count', style: TextStyle(fontSize: 34),),
+      //       ElevatedButton(onPressed: (){
+      //         setState(() {
+      //           count++;
+      //           print(count);
+      //         });
+      //       }, child: Text("Clicked me"))
+      //     ],
+      //   ),
+      // ),
+
+      body: Container(
+        color: Colors.blue.shade100,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: no1Controller,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number  ,
+                  controller: no2Controller,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(21.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                     children: [
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var sum = no1 + no2;
+                        result = "The sum of $no1 and $no2 is $sum";
+                        setState(() {
+
+                        });
+                      }, child: Text('Add')),
+
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var sub = no1 - no2;
+                        result = "The sub of $no1 and $no2 is $sub";
+                        setState(() {
+
+                        });
+                      }, child: Text('Sub')),
+
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var mul = no1 * no2;
+                        result = "The mul of $no1 and $no2 is $mul";
+                        setState(() {
+
+                        });
+                      }, child: Text('mul')),
+
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var div = no1 / no2;
+                        result = "The div of $no1 and $no2 is ${div.toStringAsFixed(2)}";
+                        setState(() {
+
+                        });
+                      }, child: Text('Div')),
+                    ],
+                  ),
+                ),
+
+                Padding(padding: EdgeInsets.all(21),
+                  child: Text('$result',style: TextStyle(fontSize: 25),  ),)
+
+              ],
+            ),
+          ),
         ),
       ),
     );
